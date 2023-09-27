5341cabf87fc4163f4af4178662f9451e2dbfd38
Monnapse <masonshuber@gmail.com>
2023-09-26 18:34:41 -0600

    update
    added global methods

--- build/lib/Linko/__init__.py
+++ build/lib/Linko/__init__.py
@@ -6,33 +6,56 @@ debug = False
 style = r'\[\[!(.*?)!\]\]'
 #style = r'\[\[! (?:.*?|\[\[!.*?!\]\])*? !\]\]'
 function = r'^\w+\(.*\)$'
-    
+
+globalMethods = {}
+
 class Linko:
-    def __init__(self, Methods: set = {}):
+    def __init__(self, isGlobal: bool = True):
+        
        """
        Connect Python and HTML to pass varius Methods.
 
        [[! valueHere !]]
        """
-       self.Methods = Methods
+
+       self.isGlobal = isGlobal
+       self.Methods = {}
+
+       #if isGlobal:
+       #    global globalMethods
+       #    globalMethods = {**globalMethods, **Methods}
+       #else:
+       #self.Methods = Methods
         
     @classmethod
-    def create_layer(cls, Methods: set = {}):
-        return cls(Methods)
+    def create_layer(cls, isGlobal: bool = False):
+        return cls(isGlobal)
 
-    def add_method(self, Method: str or types.FunctionType, Value: any or callable or None = None) -> None:
+    def add_method(self, Method: str or types.FunctionType, Value: any or callable or None = None, isGlobal: bool = False) -> None:
         """
         adds method to the Methods set
         """
+        if isGlobal or self.isGlobal:
+            global globalMethods
+            if callable(Method):
+                globalMethods[Method.__name__] = Method
+            else:
+                globalMethods[Method] = Value
+
         if callable(Method):
             self.Methods[Method.__name__] = Method
         else:
             self.Methods[Method] = Value
 
-    def render(self, content: str):
-        return render(self, content)
+    def render(self, content: str, includeGlobal: bool = True):
+        return render(self, content, includeGlobal)
 
-def render(layer, content: str) -> str:
+def render(layer, content: str, includeGlobal: bool = True) -> str:
+    Methods = layer.Methods
+
+    if includeGlobal:
+        Methods = {**globalMethods, **Methods}
+        
     matches = re.findall(style, content)
     
     for match in matches:
@@ -42,7 +65,7 @@ def render(layer, content: str) -> str:
             #function
         #else:
         try:
-            value = layer.Methods[stripped]
+            value = Methods[stripped]
             if callable(value):
                 content = content.replace(f"[[!{match}!]]", str(value()), 1)
             elif value:
@@ -53,11 +76,11 @@ def render(layer, content: str) -> str:
 
     return content
     
-def render_layers(content: str, layers: set = {}) -> str:
+def render_layers(content: str, layers: set = {}, includeGlobal: bool = True) -> str:
     new_content = content
 
     if layers != None and len(layers) > 0:
         for layer in layers:
-            new_content = render(layer, new_content)
+            new_content = render(layer, new_content, includeGlobal)
 
     return new_content
